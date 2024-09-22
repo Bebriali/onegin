0bbd7e6f5e23c2852417d86e246a27b7de280413
Bebriali <162178327+Bebriali@users.noreply.github.com>
2024-09-22 15:05:06 +0300

    Add files via upload

--- include/compare.h
+++ include/compare.h
@@ -1,1 +1,13 @@
 
+enum SORT_TYPE
+{
+    BACK = 0,
+    STRAIGHT = 1,
+    ORIGINAL = 2
+};
+
+int compare_straight(char** strings, size_t first, size_t second);
+void swap_strings(char** strings, size_t first_string, size_t second_string);
+int compare_back(char** strings, char** ends, size_t first, size_t second);
+
+void sort_strings(struct string_borders buffer_borders, bool sort_type);
---
+++ include/put_strings.h
@@ -0,0 +1,7 @@
+enum warnings
+{
+    F_ERROR,
+    ALLOC_ERROR
+};
+
+void put_strings(char** strings, size_t strings_quantity, FILE * filename);
